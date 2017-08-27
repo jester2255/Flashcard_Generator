88285873774555514f3e0d4571ac85e940496db8
jester2255 <jester2255pdunn@yahoo.com>
2017-08-26 20:34:41 -0600

    i have it working

---
+++ BasicCard.js
@@ -0,0 +1,11 @@
+/*
+The constructed object should have a front property that contains the text on the front of the card.
+The constructed object should have a back property that contains the text on the back of the card.
+*/
+function BasicCard(front,back){
+	this.front = front
+	this.back = back
+
+	
+}
+module.exports = BasicCard;
